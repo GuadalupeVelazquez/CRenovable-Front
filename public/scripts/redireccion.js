4e306e3a2ce4546c8dc4492660de9faba299dfe9
Natt24 <kevinvillagra24@gmail.com>
2024-02-01 19:21:13 -0300

    Ultimas correciones del home

--- public/scripts/redireccion.js
+++ public/scripts/redireccion.js
@@ -1,3 +1,15 @@
 function redirigir(page) {
     window.location.href = page;
 }
+
+function redirigir_decoracion() {
+    window.location.href = '/Decoracion%20de%20exteriores';
+}
+
+function redirigir_perfumes() {
+    window.location.href = '/Perfumes%20y%20fragancias'
+}
+
+function redirigir_cuidado_personal() {
+    window.location.href = '/Cuidado%20personal'
+}
